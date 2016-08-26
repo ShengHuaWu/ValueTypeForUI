a34a297d355c3b3ee1275a00584c4485050b2366
ShengHuaWu <fantasy0404@gmail.com>
2016-08-26 16:41:57 +0800

    Create ListViewController and it contains a table view.

--- ValueTypeForUI/AppDelegate.swift
+++ ValueTypeForUI/AppDelegate.swift
@@ -13,6 +13,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        let frame = UIScreen.mainScreen().bounds
+        window = UIWindow(frame: frame)
+        window?.backgroundColor = UIColor.whiteColor()
+        
+        let listViewController = ListViewController()
+        let navigationController = UINavigationController(rootViewController: listViewController)
+        window?.rootViewController = navigationController
+        
+        window?.makeKeyAndVisible()
+        
         return true
     }
 }
---
+++ ValueTypeForUI/ListViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ListViewController.swift
+//  ValueTypeForUI
+//
+//  Created by ShengHua Wu on 8/26/16.
+//  Copyright © 2016 ShengHua Wu. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    // MARK: - Lazy Initializations
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .Plain)
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.description())
+        tableView.dataSource = self
+        return tableView
+    }()
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "List"
+        
+        view.addSubview(tableView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        tableView.frame = view.bounds
+    }
+}
+
+extension ListViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(UITableViewCell.description(), forIndexPath: indexPath)
+        cell.textLabel?.text = "This is a cell."
+        
+        return cell
+    }
+}
