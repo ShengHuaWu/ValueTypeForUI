f6b28c6f38497ce9ed27e4c1966d09b3ddf42f68
ShengHuaWu <fantasy0404@gmail.com>
2016-08-26 15:54:28 +0800

    Initialize project.

---
+++ ValueTypeForUI/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  ValueTypeForUI
+//
+//  Created by ShengHua Wu on 8/26/16.
+//  Copyright © 2016 ShengHua Wu. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
---
+++ ValueTypeForUI/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ValueTypeForUI
+//
+//  Created by ShengHua Wu on 8/26/16.
+//  Copyright © 2016 ShengHua Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
